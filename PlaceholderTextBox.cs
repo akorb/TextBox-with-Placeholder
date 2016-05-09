d23dc70a9cdf4b8f52963e11292f50022b112ab9
Andreas Korb <andreas.d.korb@gmail.com>
2016-05-09 09:34:38 +0200

    Avoiding full text selection after first display with TabIndex = 0 now. See http://stackoverflow.com/a/3537816/2050020

--- PlaceholderTextBox.cs
+++ PlaceholderTextBox.cs
@@ -236,6 +236,45 @@ namespace System.Windows.Forms
         }
 
         #endregion
+
+
+        #region Avoid full text selection after first display with TabIndex = 0
+
+        // Base class has a selectionSet property, but its private.
+        // We need to shadow with our own variable. If true, this means
+        // "don't mess with the selection, the user did it."
+        bool selectionSet;
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            bool needToDeselect = false;
+
+            // We don't want to avoid calling the base implementation
+            // completely. We mirror the logic that we are trying to avoid;
+            // if the base implementation will select all of the text, we
+            // set a boolean.
+            if (!selectionSet)
+            {
+                selectionSet = true;
+
+                if (SelectionLength == 0 && MouseButtons == MouseButtons.None)
+                {
+                    needToDeselect = true;
+                }
+            }
+
+            // Call the base implementation
+            base.OnGotFocus(e);
+
+            // Did we notice that the text was selected automatically? Let's
+            // de-select it and put the caret at the end.
+            if (!needToDeselect) return;
+
+            SelectionStart = 0;
+            DeselectAll();
+        }
+
+        #endregion
     }
 
     /// <summary>
